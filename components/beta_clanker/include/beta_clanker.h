b96e5055a2c53eb5f0885677692ec5ce41f3965d
MikeOfRivia <hive.king.bee@gmail.com>
2026-09-24 16:23:02 -0500

    Add response chaining to Clanker API

--- components/beta_clanker/include/beta_clanker.h
+++ components/beta_clanker/include/beta_clanker.h
@@ -8,9 +8,10 @@ struct Result {
     bool success = false;
     int http_status = 0;
     std::string response;
+    std::string response_id;
     std::string error_code;
     std::string error_message;
 };
-Result Ask(const std::string& transcript);
+Result Ask(const std::string& transcript, const std::string& previous_response_id = {});
 }
 #endif
